65b520a063083ebe008187797d503c3af4b4ff78
LUCAS DA ROCHA MIRANDA <miranda.lucas1@gmail.com>
2023-03-01 11:37:40 -0400

    atualização do comentario

--- aula01.py
+++ aula01.py
@@ -1,15 +1,18 @@
-# {
-#    "liveServer.settings.donotShowInfoMsg": true,
-#    "explorer.compactFolders": false,
-#    "code-runner.clearPreviousOutput": true,
-#    "code-runner.runInTerminal": true,
-#    "code-runner.executorMap": {
-#        "python": "cls ; python -u",
-#    },
-#    "code-runner.ignoreSelection": true,
-#    "workbench.colorTheme": "OM Theme (Default Dracula Italic)",
-#    "workbench.iconTheme": "material-icon-theme",
-#    "python.defaultInterpreterPath": "python",
-#}
-#lembrar de alterar o executorMap de acordo com o sistema operacional
+"""
+{
+    "liveServer.settings.donotShowInfoMsg": true,
+    "explorer.compactFolders": false,
+    "code-runner.clearPreviousOutput": true,
+    "code-runner.runInTerminal": true,
+    "code-runner.executorMap": {
+        "python": "cls ; python -u",
+    },
+    "code-runner.ignoreSelection": true,
+    "workbench.colorTheme": "OM Theme (Default Dracula Italic)",
+    "workbench.iconTheme": "material-icon-theme",
+    "python.defaultInterpreterPath": "python",
+}
+lembrar de alterar o executorMap de acordo com o sistema operacional
+"""
 print ("teste")
+print ("a")
