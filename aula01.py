8b449eb32c80ab270856f106cbd36c4b77f3c362
LUCAS DA ROCHA MIRANDA <miranda.lucas1@gmail.com>
2023-03-03 16:27:24 -0400

    teste de vetor

--- aula01.py
+++ aula01.py
@@ -14,6 +14,7 @@
 }
 lembrar de alterar o executorMap de acordo com o sistema operacional
 """
+"""
 #separador
 print (12, "teste", sep='-')
 print (12, "teste",12, sep='-')
@@ -29,3 +30,35 @@ print( type('teste'))
 print( type(True))
 #converter tipo
 print(int ('1')+2)
+"""
+"""
+#f-strings
+peso = 143
+litro = 0.035
+calculo = f'{peso*litro:.2f}' 
+
+print(calculo)
+"""
+"""
+#format
+a = 'A'
+b = 'B'
+c = 14.2
+formato = 'a={} b={} c={:.2f}' .format(a, b, c)
+
+print(formato)
+"""
+#teste
+Vetor1 = []
+Vetor2 = []
+Soma = []
+soma = 0
+
+for i in range(3):
+    Vetor1.append(int(input(f'Informe um valor para o Vetor1[{i+1}]: ')))
+    Vetor2.append(int(input(f'Informe um valor para o Vetor2[{i+1}]: ')))
+    Soma.append(int(Vetor1[i] + Vetor2[i]))
+
+for i in range(3):
+    soma = soma+Soma[i]
+    print(soma)
